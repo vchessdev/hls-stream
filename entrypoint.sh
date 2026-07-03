2e206c1f0507aac70b752a557d0a18aea5e9fb65
vchessdev <dev@astraa.hs.vc>
2026-07-03 21:05:58 +0700

    Fix wget command for downloading logo image
    Updated wget command to use correct raw link format for downloading logo.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ nginx
 
 # 2. Tải file logo từ GitHub của bro về server Render
 # Lưu ý: Phải dùng link raw (raw.githubusercontent.com) thì lệnh mới tải đúng file ảnh được
-wget -O /var/www/html/logo.png https://github.com/vchessdev/hls-stream/blob/main/logo.png?raw=true
+# 2. Tải file logo từ GitHub bằng link raw chuẩn
+wget -O /var/www/html/logo.png "https://github.com/vchessdev/hls-stream/blob/main/logo.png?raw=true"
 
 # 3. LINK M3U8 NGUỒN (Thay link m3u8 bro muốn tiếp sóng vào đây)
 M3U8_URL="https://raw.githubusercontent.com/vchessdev/hls-stream/refs/heads/main/playlist.m3u8?token=GHSAT0AAAAAAEAXV6SER45NVL772TAWAQH22SHYFSA"
