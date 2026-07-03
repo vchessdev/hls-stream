75f60aee6e1445b694f6c0e1a67b5d1c658a6701
vchessdev <dev@astraa.hs.vc>
2026-07-03 21:29:49 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,17 @@
-
 #!/bin/sh
 
 # 1. Khởi động Nginx ngầm
 nginx
 
-# 2. Tải file logo từ GitHub về đúng thư mục hệ thống mới
+# 2. Tải file logo từ GitHub của bro
 wget -O /run/nginx/logo.png "https://github.com/vchessdev/hls-stream/blob/main/logo.png?raw=true"
 
 # 3. LINK M3U8 NGUỒN CỦA BRO
 M3U8_URL="https://raw.githubusercontent.com/vchessdev/hls-stream/refs/heads/main/playlist.m3u8"
 
-# 4. Chạy FFmpeg hạ độ phân giải xuống 480p để Render Free gánh nổi logo
+# 4. Giữ nguyên độ phân giải gốc + Thu nhỏ logo + Ép luồng chạy siêu tốc để cứu CPU
 ffmpeg -re -i "$M3U8_URL" -i /run/nginx/logo.png \
--filter_complex "[0:v]fps=20,scale=854:480[bg];[bg][1:v]overlay=main_w-overlay_w-15:main_h-overlay_h-15" \
--c:v libx264 -preset ultrafast -tune zerolatency -crf 30 -maxrate 600k -bufsize 1200k \
--c:a aac -b:a 64k \
+-filter_complex "[1:v]scale=150:-1[logo];[0:v][logo]overlay=main_w-overlay_w-20:main_h-overlay_h-20" \
+-c:v libx264 -preset ultrafast -tune zerolatency -threads 1 -crf 26 -maxrate 1200k -bufsize 2400k \
+-c:a aac -b:a 96k \
 -f flv rtmp://localhost/hls_live/live247
-
