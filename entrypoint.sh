8890d3cc100e293a21f06b4fbcdbca42ff9d988b
vchessdev <dev@astraa.hs.vc>
2026-07-03 21:11:51 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,16 @@
 
 # 1. Khởi động Nginx ngầm
 nginx
-# 3. LINK M3U8 NGUỒN (Thay link m3u8 bro muốn tiếp sóng vào đây)
-M3U8_URL="https://raw.githubusercontent.com/vchessdev/hls-stream/refs/heads/main/playlist.m3u8?token=GHSAT0AAAAAAEAXV6SER45NVL772TAWAQH22SHYFSA"
-# Chạy FFmpeg hạ cấu hình tối đa để Render Free gánh nổi
-# 3. Dùng lệnh copy thuần túy (siêu nhẹ, bao mượt cho Render Free)
-ffmpeg -re -i "$M3U8_URL" -c:v copy -c:a copy -f flv rtmp://localhost/hls_live/live247
+
+# 2. Tải file logo từ GitHub của bro
+wget -O /var/www/html/logo.png "https://github.com/vchessdev/hls-stream/blob/main/logo.png?raw=true"
+
+# 3. LINK M3U8 NGUỒN CỦA BRO
+M3U8_URL="https://raw.githubusercontent.com/vchessdev/hls-stream/refs/heads/main/playlist.m3u8"
+
+# 4. ÉP HẠ ĐỘ PHÂN GIẢI XUỐNG 480P + GIẢM KHUNG HÌNH (FPS) ĐỂ CỨU CPU
+ffmpeg -re -i "$M3U8_URL" -i /var/www/html/logo.png \
+-filter_complex "[0:v]fps=20,scale=854:480[bg];[bg][1:v]overlay=main_w-overlay_w-15:main_h-overlay_h-15" \
+-c:v libx264 -preset ultrafast -tune zerolatency -crf 30 -maxrate 600k -bufsize 1200k \
+-c:a aac -b:a 64k \
+-f flv rtmp://localhost/hls_live/live247
